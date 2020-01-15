269d0fe8158950fed971235eec79cf6c51f99baa
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2020-01-15 10:08:55 -0300

    Create OPEN_DOCKER.bat

---
+++ OPEN_DOCKER.bat
@@ -0,0 +1,29 @@
+REM :bye
+REM ECHO BYE BYE
+REM pause
+REM goto end
+REM :test
+REM ECHO TEST
+REM goto end
+REM ECHO 2. Print Bye
+REM ECHO 3. Print Test
+REM if '%choice%'=='2' goto bye
+REM if '%choice%'=='3' goto test
+@ECHO off
+cls
+:start
+ECHO.
+ECHO Gostaria de iniciar o skype?
+set /p choice=Escolha s/n:  
+rem if not '%choice%'=='' set choice=%choice:~0;1% (don`t use this command, because it takes only first digit in the case you type more digits. After that for example choice 23455666 is choice 2 and you get "bye"
+if not '%choice%'=='s' if not '%choice%'=='S'  goto end
+if '%choice%'=='s' goto software
+if '%choice%'=='S' goto software
+goto start
+:software
+REM cd "C:\Program Files\Docker\Docker\Docker Desktop.exe" 
+REM start "Docker Desktop.exe"
+"C:\Program Files\Docker\Docker\Docker Desktop.exe"
+goto end
+:end
+exit
